4029d55cdedb0519094240f6a6c3b6bb565dc33c
merijnvervoorn <161606054+merijnvervoorn@users.noreply.github.com>
2025-01-08 23:29:52 +0100

    Adding terminal settings

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,10 @@
+rm -rf ~/.alias
+rm -rf ~/.bashrc
+rm -rf ~/.functions
+rm -rf ~/.gitconfig
+rm -rf ~/.inputrc
+rm -rf ~/.profile
+
 ln -s ~/.dotfiles/.alias ~/.alias
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.functions ~/.functions
