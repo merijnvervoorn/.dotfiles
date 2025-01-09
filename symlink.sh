fc9982aa14488016f201769c9186576f0c00c85e
merijnvervoorn <161606054+merijnvervoorn@users.noreply.github.com>
2025-01-09 14:01:30 +0100

    Gnome extensions and aliases

--- symlink.sh
+++ symlink.sh
@@ -4,7 +4,7 @@ rm -rf ~/.functions
 rm -rf ~/.gitconfig
 rm -rf ~/.inputrc
 rm -rf ~/.profile
-rm -rf ~/.local/share/gnome-shell
+rm -rf ~/.local/share/gnome-shell/extensions
 
 ln -s ~/.dotfiles/.alias ~/.alias
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
@@ -12,4 +12,4 @@ ln -s ~/.dotfiles/.functions ~/.functions
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.inputrc ~/.inputrc
 ln -s ~/.dotfiles/.profile ~/.profile
-ln -s ~/.dotfiles/gnome-shell ~/.local/share/gnome-shell
+ln -s ~/.dotfiles/gnome-extensions ~/.local/share/gnome-shell/extensions
