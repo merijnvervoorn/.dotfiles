5dd708c915e04d977f713a3368cb73b9863f98f6
merijnvervoorn <161606054+merijnvervoorn@users.noreply.github.com>
2025-01-15 11:11:00 +0100

    Adding fonts the right way

--- symlink.sh
+++ symlink.sh
@@ -6,7 +6,7 @@ rm -rf ~/.inputrc
 rm -rf ~/.profile
 rm -rf ~/.local/share/gnome-shell/extensions
 rm -rf ~/.local/share/fonts/other
-rm -rf /usr/share/fonts/other
+sudo rm -rf /usr/share/fonts/other
 
 ln -s ~/.dotfiles/.alias ~/.alias
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
@@ -16,4 +16,4 @@ ln -s ~/.dotfiles/.inputrc ~/.inputrc
 ln -s ~/.dotfiles/.profile ~/.profile
 ln -s ~/.dotfiles/gnome-extensions ~/.local/share/gnome-shell/extensions
 ln -s ~/.dotfiles/fonts ~/.local/share/fonts
-ln -s ~/.dotfiles/fonts /usr/share/fonts/other
+sudo ln -s ~/.dotfiles/fonts /usr/share/fonts/other
