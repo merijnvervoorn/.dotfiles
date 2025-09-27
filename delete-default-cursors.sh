129a2269d7080d2c837da8a03eafab141a384240
Merijn <161606054+merijnvervoorn@users.noreply.github.com>
2025-09-27 15:01:20 +0200

    Adding more cursors

---
+++ delete-default-cursors.sh
@@ -0,0 +1,7 @@
+sudo rm -rf /usr/share/icons/handhelds
+sudo rm -rf /usr/share/icons/DMZ-Black
+sudo rm -rf /usr/share/icons/DMZ-White
+sudo rm -rf /usr/share/icons/breeze_cursors
+sudo rm -rf /usr/share/icons/Breeze_Snow
+sudo rm -rf /usr/share/icons/redglass
+sudo rm -rf /usr/share/icons/whiteglass
